da9038aadaad9e005999227118b8facf1c440035
Qch <2368831528@qq.com>
2023-08-17 12:49:29 +0800

    fix:Test source

--- USER/main.c
+++ USER/main.c
@@ -1,12 +1,12 @@
 #include "sys.h"
 
- int main(void)
- {	
-	 delay_init();
-	 
-	 while(1)
-	 {
-		 
-	 }
- }
+int main(void)
+{
+	delay_init();
 
+	while (1)
+	{
+		printf("hello world\r\n");
+		delay_ms(1000);
+	}
+}
